17ba32f37b28ea40f28b46dcbcd5471ee6e28d1d
BeverleyB <beverley.beranger@gmail.com>
2021-07-21 19:10:21 +0200

    Modification du backend

--- server/app.js
+++ server/app.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import bodyparser from 'body-parser';
-import cors from 'cors';
-import nodemailer from 'nodemailer';
-import ejs from 'ejs';
-import { google } from 'googleapis';
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+const { google } = require("googleapis");
 
 const { OAuth2 } = google.auth;
 
 const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
-
-const app = express();
 
 const {
   MAILING_SERVICE_CLIENT_ID,
@@ -17,35 +16,48 @@ const {
   MAILING_SERVICE_REFRESH_TOKEN,
   SENDER_EMAIL_ADDRESS,
 } = process.env;
-
-const oauth2Client = new OAuth2(
-  MAILING_SERVICE_CLIENT_ID,
-  MAILING_SERVICE_CLIENT_SECRET,
-  OAUTH_PLAYGROUND
-);
   
 app.use(cors({ origin: "*" }));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 app.post("/sendmail", (req, res) => {
-  oauth2Client.setCredentials({
-    refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
-  });
-  const accessToken = oauth2Client.getAccessToken();
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: SENDER_EMAIL_ADDRESS,
-      clientId: MAILING_SERVICE_CLIENT_ID,
-      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-      refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-      accessToken,
-    },
-  });
+  const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+      MAILING_SERVICE_CLIENT_ID,
+      MAILING_SERVICE_CLIENT_SECRET,
+      OAUTH_PLAYGROUND
+    );
 
+    oauth2Client.setCredentials({
+      refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
+    });
+
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject("Failed to create access token :(");
+        }
+        resolve(token);
+      });
+    });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: SENDER_EMAIL_ADDRESS,
+        clientId: MAILING_SERVICE_CLIENT_ID,
+        clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+        refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+        accessToken,
+        expires: 1484314697598
+      },
+    });
+
+    return transporter;
+  };
 
   ejs.renderFile('./templates/email-tmp.ejs', { email: req.body.email, message: req.body.message }, {}, (e, content) => {
     if (e) return e;
@@ -59,15 +71,19 @@ app.post("/sendmail", (req, res) => {
       html: content,
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
+    const sendEmail = async (emailOptions) => {
+      try {
+        let emailTransporter = await createTransporter();
+        let info = await emailTransporter.sendMail(emailOptions);
+        console.log("Message sent: %s", info.messageId);
+        res.send(info);
       }
-      console.log("Message sent: %s", info.messageId);
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      res.send(info);
-    });
+      catch(error) {
+        console.log(error);
+      }
+    };
 
+    sendEmail(mailOptions);
   });
 });
 
--- server/dist/app.js
+++ server/dist/app.js
@@ -1,62 +1,91 @@
 "use strict";
 
-var _express = _interopRequireDefault(require("express"));
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-var _bodyParser = _interopRequireDefault(require("body-parser"));
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var _cors = _interopRequireDefault(require("cors"));
+var express = require("express");
 
-var _nodemailer = _interopRequireDefault(require("nodemailer"));
+var bodyparser = require("body-parser");
 
-var _ejs = _interopRequireDefault(require("ejs"));
+var cors = require("cors");
 
-var _googleapis = require("googleapis");
+var app = express();
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var nodemailer = require("nodemailer");
 
-// const express = require("express");
-// const bodyparser = require("body-parser");
-// const cors = require("cors");
-// const router = express.Router();
-// const nodemailer = require("nodemailer");
-// const ejs = require('ejs');
-// const { google } = require("googleapis");
-var OAuth2 = _googleapis.google.auth.OAuth2;
+var ejs = require('ejs');
+
+var _require = require("googleapis"),
+    google = _require.google;
+
+var OAuth2 = google.auth.OAuth2;
 var OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
-var app = (0, _express["default"])();
 var _process$env = process.env,
     MAILING_SERVICE_CLIENT_ID = _process$env.MAILING_SERVICE_CLIENT_ID,
     MAILING_SERVICE_CLIENT_SECRET = _process$env.MAILING_SERVICE_CLIENT_SECRET,
     MAILING_SERVICE_REFRESH_TOKEN = _process$env.MAILING_SERVICE_REFRESH_TOKEN,
     SENDER_EMAIL_ADDRESS = _process$env.SENDER_EMAIL_ADDRESS;
-console.log(MAILING_SERVICE_CLIENT_ID);
-var oauth2Client = new OAuth2(MAILING_SERVICE_CLIENT_ID, MAILING_SERVICE_CLIENT_SECRET, OAUTH_PLAYGROUND);
-app.use((0, _cors["default"])({
+app.use(cors({
   origin: "*"
 }));
-app.use(_bodyParser["default"].urlencoded({
+app.use(bodyparser.urlencoded({
   extended: true
 }));
-app.use(_bodyParser["default"].json());
+app.use(bodyparser.json());
 app.post("/sendmail", function (req, res) {
-  oauth2Client.setCredentials({
-    refresh_token: MAILING_SERVICE_REFRESH_TOKEN
-  });
-  var accessToken = oauth2Client.getAccessToken();
+  var createTransporter = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+      var oauth2Client, accessToken, transporter;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              oauth2Client = new OAuth2(MAILING_SERVICE_CLIENT_ID, MAILING_SERVICE_CLIENT_SECRET, OAUTH_PLAYGROUND);
+              oauth2Client.setCredentials({
+                refresh_token: MAILING_SERVICE_REFRESH_TOKEN
+              });
+              _context.next = 4;
+              return new Promise(function (resolve, reject) {
+                oauth2Client.getAccessToken(function (err, token) {
+                  if (err) {
+                    reject("Failed to create access token :(");
+                  }
 
-  var transporter = _nodemailer["default"].createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: SENDER_EMAIL_ADDRESS,
-      clientId: MAILING_SERVICE_CLIENT_ID,
-      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-      refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-      accessToken: accessToken
-    }
-  });
+                  resolve(token);
+                });
+              });
 
-  _ejs["default"].renderFile('./templates/email-tmp.ejs', {
+            case 4:
+              accessToken = _context.sent;
+              transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                  type: "OAuth2",
+                  user: SENDER_EMAIL_ADDRESS,
+                  clientId: MAILING_SERVICE_CLIENT_ID,
+                  clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+                  refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+                  accessToken: accessToken,
+                  expires: 1484314697598
+                }
+              });
+              return _context.abrupt("return", transporter);
+
+            case 7:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+
+    return function createTransporter() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+
+  ejs.renderFile('./templates/email-tmp.ejs', {
     email: req.body.email,
     message: req.body.message
   }, {}, function (e, content) {
@@ -68,15 +97,49 @@ app.post("/sendmail", function (req, res) {
       subject: 'Un nouveau message sur ton site <3',
       html: content
     };
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        return console.log(error);
-      }
 
-      console.log("Message sent: %s", info.messageId);
-      console.log("Preview URL: %s", _nodemailer["default"].getTestMessageUrl(info));
-      res.send(info);
-    });
+    var sendEmail = /*#__PURE__*/function () {
+      var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(emailOptions) {
+        var emailTransporter, info;
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                _context2.prev = 0;
+                _context2.next = 3;
+                return createTransporter();
+
+              case 3:
+                emailTransporter = _context2.sent;
+                _context2.next = 6;
+                return emailTransporter.sendMail(emailOptions);
+
+              case 6:
+                info = _context2.sent;
+                console.log("Message sent: %s", info.messageId);
+                res.send(info);
+                _context2.next = 14;
+                break;
+
+              case 11:
+                _context2.prev = 11;
+                _context2.t0 = _context2["catch"](0);
+                console.log(_context2.t0);
+
+              case 14:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2, null, [[0, 11]]);
+      }));
+
+      return function sendEmail(_x) {
+        return _ref2.apply(this, arguments);
+      };
+    }();
+
+    sendEmail(mailOptions);
   });
 });
 app.listen('3000', function () {
